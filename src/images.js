9ca8aa0a8d21bbc9963c967c6c1ec9c9b076d8dc
João Amaral <jp.amaral@ua.pt>
2023-05-18 14:47:30 +0100

    updated image path

--- src/images.js
+++ src/images.js
@@ -1,17 +1,17 @@
 // images.js
 const images = {
     "U": require("../assets/images/U.png"),
-    "NU": require("../assets/images/U'.png"),
+    "NU": require("../assets/images/NU.png"),
     "B": require("../assets/images/B.png"),
-    "NB": require("../assets/images/B'.png"),
+    "NB": require("../assets/images/NB.png"),
     "L": require("../assets/images/L.png"),
-    "NL": require("../assets/images/L'.png"),
+    "NL": require("../assets/images/NL.png"),
     "R": require("../assets/images/R.png"),
-    "NR": require("../assets/images/R'.png"),
+    "NR": require("../assets/images/NR.png"),
     "F": require("../assets/images/F.png"),
-    "NF": require("../assets/images/F'.png"),
+    "NF": require("../assets/images/NF.png"),
     "D": require("../assets/images/D.png"),
-    "ND": require("../assets/images/D'.png"),
+    "ND": require("../assets/images/ND.png"),
   };
   
   export default images;
